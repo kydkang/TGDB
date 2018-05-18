238ae0337fbad940feeab79c5eb78c64592f4fa8
KYD <kydkang@gmail.com>
2018-05-18 07:44:32 +0700

    branch 9a commit

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse 
 import datetime 
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello Django")
     
 def today_is(request):
     now = datetime.datetime.now()
-    html = "<html><body>Current date and time:{0}</body></html>".format(now)
-    return HttpResponse(html)
+    return render(request, 'blog/datetime.html', {'now':now})
 
