8884dd2a5cadb28f32e6a3fe86bfa4f9b5e6643a
KYD <kydkang@gmail.com>
2018-05-23 08:30:33 +0700

    21a branch commit

---
+++ cadmin/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('post/add/', views.post_add, name='post_add'), 
+    path('post/update/<int:pk>/', views.post_update, name='post_update'), 
+]
--- django_project/urls.py
+++ django_project/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('', include('blog.urls')),  
     path('admin/', admin.site.urls),  
+    path('cadmin/', include('cadmin.urls')), 
 ]
