85b060bc2f606459d17a2679df06ec8d5ac45056
KYD <kydkang@gmail.com>
2018-05-25 10:54:19 +0700

    33a branch commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from . import models 
 
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'created_on')
-    search_fields = ['name', 'email']
-    ordering = ['-name']
-    list_filter = ['active']
-    date_hierarchy = 'created_on'
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'email', 'created_on')
+#     search_fields = ['name', 'email']
+#     ordering = ['-name']
+#     list_filter = ['active']
+#     date_hierarchy = 'created_on'
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'pub_date', 'author', 'category',)
@@ -15,6 +15,7 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     date_hierarchy = 'pub_date'    
     filter_horizontal =('tags',)
+    #raw_id_fields =('tags',)
     #prepopulated_fields = {'slug':('title',)}
     readonly_fields =('slug',)
     fields = ('title', 'slug', 'content', 'author', 'category', 'tags',)
@@ -34,6 +35,7 @@ class FeedbackAdmin(admin.ModelAdmin):
 
 admin.site.register(models.Post, PostAdmin)
 admin.site.register(models.Category, CategoryAdmin)
-admin.site.register(models.Author, AuthorAdmin)
+# admin.site.register(models.Author, AuthorAdmin)
+admin.site.register(models.Author) 
 admin.site.register(models.Tag, TagAdmin)
 admin.site.register(models.Feedback, FeedbackAdmin)
--- cadmin/urls.py
+++ cadmin/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('activate/account/', views.activate_account, name='activate'),
     path('register/', views.register, name='register'), 
     path('password-change-done/', auth_views.password_change_done, 
           {'template_name':'cadmin/password_change_done.html'}, 
@@ -12,6 +13,7 @@ urlpatterns = [
           name='password_change'),
     path('', views.home, name='home'),  
     path('login/', views.login, {'template_name': 'cadmin/login.html'}, name='login'),
+    path('logout/', auth_views.logout, {'template_name': 'cadmin/logout.html'}, name='logout'),
     path('post/add/', views.post_add, name='post_add'), 
     path('post/update/<int:pk>/', views.post_update, name='post_update'), 
 ]
